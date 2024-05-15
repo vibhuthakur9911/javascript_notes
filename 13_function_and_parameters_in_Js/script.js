b5c27c527bdd8a21cdd5e9ea9d1a1be85af2fb15
Vaibhav <varunrajput12o8@gmail.com>
2024-05-15 17:32:46 +0530

    Function with object and rest parameter in Js.

--- 13_function_and_parameters_in_Js/script.js
+++ 13_function_and_parameters_in_Js/script.js
@@ -67,3 +67,54 @@ function anotherUser_Name(userName = "Vibhu Thakur") {
 
 console.log(anotherUser_Name()); // Welcome, Vibhu Thakur
 console.log(anotherUser_Name("Vaibhav Thakur")); // Welcome, Vaibhav Thakur
+
+// Arguments:
+
+// #Arguments are the actual values passed to a function when it is called.
+// #They correspond to the parameters defined in the function declaration.
+// #Arguments are provided within the parentheses () when calling the function.
+
+function greet(name) {
+  return name;
+}
+
+let arg = greet("Vaibhav"); // 'Vaibhav' is an argument
+
+// Rest Parameters
+
+// #Rest parameters allow functions to accept an indefinite number of arguments as an array.
+
+// #They are specified using the rest parameter syntax `....`
+
+function nums(num) {
+  return num;
+}
+
+console.log(nums(100)); // Output: 100
+console.log(nums(100, 200, 300)); //Output:  100  , Eventully It will return the first value
+
+// To tackle the above condition we can use rest parameters.
+
+function calculatNum(...numbers) {
+  return numbers;
+}
+
+console.log(calculatNum(100, 200, 300)); // Output : [ 100, 200, 300 ]
+
+const new_course = {
+  title: "Js",
+  instructorName: "Vaibhav",
+};
+
+function handleObject(anyobject) {
+  return `This is ${anyobject.title} Course and instrcutor will be ${anyobject.instructorName}`;
+}
+
+console.log(handleObject(new_course));
+
+console.log(
+  handleObject({
+    title: "React",
+    instructorName: "Vibhu",
+  })
+);
