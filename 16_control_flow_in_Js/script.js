5494a1be0555f6415e293a8bc5b5f5cd5d071a81
Vaibhav <varunrajput12o8@gmail.com>
2024-05-22 17:31:00 +0530

    Control flow interruption.

--- 16_control_flow_in_Js/script.js
+++ 16_control_flow_in_Js/script.js
@@ -8,3 +8,18 @@ x += 5; // x is now 15
 console.log(x); // Output: 15
 
 // We will discuss more topic about control flow in dedicated notes.
+
+// Control Flow Interruptions:
+// break Statement: Exits a loop or switch statement prematurely.
+
+for (let i = 0; i < 10; i++) {
+  if (i === 5) break;
+  console.log(i); // Output: 0, 1, 2, 3, 4
+}
+
+// continue Statement: Skips the current iteration of a loop and proceeds to the next iteration.
+
+for (let i = 0; i < 10; i++) {
+  if (i === 5) continue;
+  console.log(i); // Output: 0, 1, 2, 3, 4, 6, 7, 8, 9
+}
