ab79ec19e58d39e325ede9ae7be4658d496bc0c6
Vaibhav <varunrajput12o8@gmail.com>
2024-05-22 17:25:54 +0530

    Control flow in javascript.

---
+++ 16_control_flow_in_Js/script.js
@@ -0,0 +1,10 @@
+// Control flow in JavaScript refers to the order in which individual statements, instructions, or function calls are executed or evaluated. Understanding control flow is essential for writing efficient and effective programs.
+
+// Sequential Execution:
+// By default, JavaScript code runs from the first statement to the last in a sequential manner.
+
+let x = 10;
+x += 5; // x is now 15
+console.log(x); // Output: 15
+
+// We will discuss more topic about control flow in dedicated notes.
