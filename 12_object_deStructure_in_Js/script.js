31ed7a66f54df03834fe0e28b1d9f008b1dfc24c
Vaibhav <varunrajput12o8@gmail.com>
2024-05-28 12:16:32 +0530

    Prettify  a file.

--- 12_object_deStructure_in_Js/script.js
+++ 12_object_deStructure_in_Js/script.js
@@ -3,11 +3,11 @@
 // It allows you to extract values from objects and bind them to variables using a syntax that mirrors the structure of the object.
 
 const course = {
-    courseName : "Javascript for beginners",
-    price : 999,
-    courseInstructor : 'Vaibhav Thakur',
-    chaptersIncludedInTher: 66
-}
+  courseName: "Javascript for beginners",
+  price: 999,
+  courseInstructor: "Vaibhav Thakur",
+  chaptersIncludedInTher: 66,
+};
 
 // Object destructuring is performed using curly braces {} on the left-hand side of an assignment.
 
@@ -15,32 +15,36 @@ const course = {
 
 // let { prop1, prop2 } = object;
 
-let {courseName} = course;
+let { courseName } = course;
 console.log(courseName); // Output: Javascript for beginners
 
-let { price, courseInstructor} = course;
-console.log(price)              // Output: 999
-console.log(courseInstructor)   // Output: Vaibhav Thakur
+let { price, courseInstructor } = course;
+console.log(price); // Output: 999
+console.log(courseInstructor); // Output: Vaibhav Thakur
 
-let {chaptersIncludedInTher: chapter} = course;
-console.log(chapter);  // Output: 66
+let { chaptersIncludedInTher: chapter } = course;
+console.log(chapter); // Output: 66
 
 // Nested Object Destructuring:
 
 let another_course = {
-    name : "ReactJS Course",
-    duration : "30 weeks",
-    instructor : {
-        name : "Vaibhav Thakur",
-        age : 22,
-        country : "India"
-        },
-}
+  name: "ReactJS Course",
+  duration: "30 weeks",
+  instructor: {
+    name: "Vaibhav Thakur",
+    age: 22,
+    country: "India",
+  },
+};
 
-let {name, duration, instructor: {name: instructorName, age, country}} = another_course
+let {
+  name,
+  duration,
+  instructor: { name: instructorName, age, country },
+} = another_course;
 
-console.log(name);    // output: ReactJS Course
-console.log(duration);// output: 30 weeks
-console.log(instructorName)  // output: Vaibhav Thakur
-console.log(age)  // output: 22
-console.log(country)  // output: India
+console.log(name); // output: ReactJS Course
+console.log(duration); // output: 30 weeks
+console.log(instructorName); // output: Vaibhav Thakur
+console.log(age); // output: 22
+console.log(country); // output: India
